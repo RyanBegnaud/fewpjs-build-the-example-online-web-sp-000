d9700d73acef86ba9640dbeec395316cac114932
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-12-06 00:50:07 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let jsHeart = document.getElementsByClassName("like-glyph")[0]
 jsHeart.addEventListener("click", function(e){
   if (e.target.innerText === EMPTY_HEART) {
     e.target.innerText = FULL_HEART;
-    e.target.style.color = "blue";
+    e.target.style.background = "blue";
   }
   
   if (e.target.innerText === FULL_HEART) {
